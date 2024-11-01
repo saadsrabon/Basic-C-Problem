0b8440b1f568d5ee39f6de7b2d9daea9b83105a3
Saad Srabon <saadsrabon2@gmail.com>
2024-11-02 05:06:19 +0600

    solved count letters problem

---
+++ countLetter.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main ()
+{
+  char S[30];
+  scanf("%s", &S);
+
+
+//   i need to count the frequency of each letter in the string
+
+    int freq[26]={0};
+    
+    
+    for(int i=0; S[i]!='\0'; i++)
+    {
+        freq[S[i] - 'a'] += 1;
+    }
+    
+    for(int i=0; i<26; i++)
+    {
+        if(freq[i] > 0){
+
+        printf("%c : %d\n", i+'a', freq[i]);
+
+        }
+    }
+}
